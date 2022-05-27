5c6fb14219de7568c9cf8bf162dbc3491ed4a2ad
Franz Pamorada <57946910+frnzpmrd@users.noreply.github.com>
2022-05-28 00:22:39 +0800

    Test the leg doing 30 degree raises

--- Leg_Therapy.ino
+++ Leg_Therapy.ino
@@ -11,19 +11,17 @@ void setup() {
   pinMode(motor2pin1, OUTPUT);
   pinMode(motor2pin2, OUTPUT);
 
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:   
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
   digitalWrite(motor2pin1, HIGH);
   digitalWrite(motor2pin2, LOW);
-  delay(1000);
+  delay(250);
   digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
+  digitalWrite(motor1pin2, LOW);
   digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-  delay(1000);
+  digitalWrite(motor2pin2, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:   
 }
