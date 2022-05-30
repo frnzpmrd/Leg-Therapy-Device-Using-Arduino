4e264e4fd1185dd2769909d80fb64b3d465fba57
Franz Pamorada <57946910+frnzpmrd@users.noreply.github.com>
2022-05-30 22:31:22 +0800

    Test 30 Degree Calf Raises using button as trigger

--- Leg_Therapy.ino
+++ Leg_Therapy.ino
@@ -6,22 +6,35 @@ int motor2pin2 = 5;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(12, INPUT);
+  pinMode(13, INPUT);
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
   pinMode(motor2pin1, OUTPUT);
   pinMode(motor2pin2, OUTPUT);
 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int temp = digitalRead(12);
+  int temp1 = digitalRead(13);
+
+  if(temp == HIGH){
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
+  delay(1500);
+
+    }
+  else if(temp1 == HIGH){
   digitalWrite(motor2pin1, HIGH);
   digitalWrite(motor2pin2, LOW);
-  delay(250);
+  delay(1500);
+    }
+  else{
   digitalWrite(motor1pin1, LOW);
   digitalWrite(motor1pin2, LOW);
   digitalWrite(motor2pin1, LOW);
   digitalWrite(motor2pin2, LOW);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:   
+    }
 }
