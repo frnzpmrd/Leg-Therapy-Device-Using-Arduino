67ab2f5d58038a8ffcd88d7bad2368a1c14af3f3
Franz Pamorada <57946910+frnzpmrd@users.noreply.github.com>
2022-06-06 02:35:21 +0800

    Full implementation of all devices

--- Leg_Therapy.ino
+++ Leg_Therapy.ino
@@ -1,48 +1,14 @@
-/*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
-
-*/
-
 // include the library code:
 #include <LiquidCrystal.h>
+const int Button1 = 26;
+const int Button2 = 28;
+const int Button3 = 30;
+const int Button4 = 32;
 
+int motor1pin1 = 36;
+int motor1pin2 = 38;
+int motor2pin1 = 40;
+int motor2pin2 = 42;
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
@@ -50,7 +16,10 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
   // set up the LCD's number of columns and rows:
-  
+  pinMode(Button1, INPUT);
+  pinMode(Button2, INPUT);
+  pinMode(Button3, INPUT);
+  pinMode(Button4, INPUT);
   lcd.begin(16, 4);
   // Print a message to the LCD.
   lcd.setCursor(1,0);
@@ -67,8 +36,10 @@ void setup() {
 void loop() {
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
-  int temp = digitalRead(6);
-  Serial.println(temp);
+  int temp = digitalRead(Button1);
+  int temp1 = digitalRead(Button2);
+  int temp2 = digitalRead(Button3);
+  int temp3 = digitalRead(Button4);
   
   if(temp == 1){
   lcd.clear();
@@ -80,9 +51,63 @@ void loop() {
   lcd.print("30 Degree Left");
   lcd.setCursor(1,3);
   lcd.print("Calf Raises");
-
-
-  delay(5000);
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  delay(1500);
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+  delay(1500);
+    }
+  else if(temp1 == 1){
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print("Starting");
+  lcd.setCursor(0,1);
+  lcd.print("----------------");
+  lcd.setCursor(1,2);
+  lcd.print("30 Degree Right");
+  lcd.setCursor(1,3);
+  lcd.print("Calf Raises");
+  digitalWrite(motor2pin2, HIGH);
+  digitalWrite(motor2pin1, LOW);
+  delay(1500);
+  digitalWrite(motor1pin2, LOW);
+  digitalWrite(motor1pin1, HIGH);
+  delay(1500);
+    }
+  else if(temp2 == 1){
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print("Starting");
+  lcd.setCursor(0,1);
+  lcd.print("----------------");
+  lcd.setCursor(1,2);
+  lcd.print("60 Degree Left");
+  lcd.setCursor(1,3);
+  lcd.print("Calf Raises");
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  delay(2500);
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+  delay(2500);
+    }
+  else if(temp3 == 1){
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print("Starting");
+  lcd.setCursor(0,1);
+  lcd.print("----------------");
+  lcd.setCursor(1,2);
+  lcd.print("6 0 Degree Right");
+  lcd.setCursor(1,3);
+  lcd.print("Calf Raises");
+  digitalWrite(motor2pin2, HIGH);
+  digitalWrite(motor2pin1, LOW);
+  delay(2500);
+  digitalWrite(motor1pin2, LOW);
+  digitalWrite(motor1pin1, HIGH);
+  delay(2500);
     }
   else{
   lcd.clear();
@@ -128,4 +153,3 @@ void loop() {
     }
 
 }
-
